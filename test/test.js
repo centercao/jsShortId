8e857aa59c4c8598ff8a42cbd0f9119b0b2b3da3
centercao <centercao2010@163.com>
2019-02-22 12:42:31 +0800

    支持浏览器

--- test/test.js
+++ test/test.js
@@ -1,20 +1,26 @@
 /**
  * Created By Center on 2018/2/3
  **/
-const ShortId =require("../index");
-const shortId =new ShortId();
+const shortId =require("../jsShortId");
 // console.log(shortId.gen(13));
-let list = new Set();
-let start = +new Date();
+let set = new Set();
 // console.log(new Date().format("yyyy-MM-dd hh:mm:ss.S"));
-for(let i =0 ;i< 1;i++){
+let num = 1000000;
+let da = shortId.gen(15);
+console.log(da);
+console.log(shortId.getDate(da,"yyyy-MM-dd hh:mm:ss"));
+console.log(shortId.gen(15));
+console.log(shortId.gen(15));
+console.log(shortId.gen(15));
+let start = +new Date();
+for(let i =0 ;i< num;i++){
 	let id =shortId.gen(15);
-	list.add(id);
+	set.add(id);
 	//console.log(id);
-	console.log(shortId.getDate(id,"yyyy-MM-dd hh:mm:ss"));
+	// console.log(shortId.getDate(id,"yyyy-MM-dd hh:mm:ss"));
 }
-console.log(+new Date()-start);
-console.log(list.size);
+console.log('耗时:' + (+new Date()-start));
+console.log('产生数量:' + num + '有效数量:' + set.size);
 
 /*let num = +new Date("2018-12-31 23:59:59");
 console.log(num.toString(36));*/
